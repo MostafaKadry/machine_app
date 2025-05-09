a9cbf523dd4e8e595931c973a739905d43ba4468
MostafaKadry <mostafakadry806@gmail.com>
2025-05-09 04:39:24 +0300

    fix typos in scheduler task

--- machine_seller/tasks.py
+++ machine_seller/tasks.py
@@ -3,10 +3,10 @@ import frappe
 def mark_machines_as_sold():
     unsold_machines = frappe.get_all(
         "Machine",
-        filters={"selling_status": "Unsold"},
+        filters={"selling_status": "unsold"},
         fields=["name"]
     )
     if not unsold_machines:
         return
     for machine in unsold_machines:
-        frappe.db.set_value("Machine", machine.name, "selling_status", "Sold")
+        frappe.db.set_value("Machine", machine.name, "selling_status", "sold")
